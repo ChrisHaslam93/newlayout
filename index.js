09112a08f0344b51417efe73785eab11804293f7
Chris Haslam <80816771+ChrisHaslam93@users.noreply.github.com>
2023-10-25 18:00:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// const resetAllTabColours = () => {
+//     document.querySelectorAll('.tab-selection').forEach(t => t.classList.remove('tab-selected'));
+// }
+
+// let tabMenu = document.querySelector('.tab-menu');
+// tabMenu.addEventListener('click', (e) => {
+//     resetAllTabColours();
+//     e.target.classList.add('tab-selected');
+//     displayCorrectPageContent(e);
+// });
+
+
+// const displayCorrectPageContent = (e) => {
+//     document.querySelectorAll('#page_content .page-tab').forEach(t => t.classList.add('hidden'));
+//     let selectedTab = document.querySelector('.' + e.target.innerText.toLowerCase().replace(' ', '-'));
+//     selectedTab.classList.remove('hidden');
+// }
+
